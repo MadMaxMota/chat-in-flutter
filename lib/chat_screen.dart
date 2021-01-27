442ce99493ce17eb7cc134a65a3558f1113d4498
YuriMotaSilva <yurimotasilva1@gmail.com>
2021-01-26 21:28:20 -0300

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
+import 'package:chat_in_flutter/chat_message.dart';
 import 'package:chat_in_flutter/text_composer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -11,8 +13,54 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  FirebaseUser _currentUser;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<FirebaseUser> getUser() async {
+    if (_currentUser != null) return _currentUser;
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken,
+      );
+
+      final AuthResult authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final FirebaseUser user = authResult.user;
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
   Future<void> _sendMessage({String text, File imgFile}) async {
-    Map<String, dynamic> data = {};
+    final FirebaseUser user = await getUser();
+
+    if (user == null) {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Não foi possivel fazer o login. Tente novamente!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+    Map<String, dynamic> data = {
+      'uid': user.uid,
+      'senderName': user.displayName,
+      'senderPhotoUrl': user.photoUrl,
+    };
 
     if (imgFile != null) {
       StorageUploadTask task = FirebaseStorage.instance
@@ -27,12 +75,13 @@ class _ChatScreenState extends State<ChatScreen> {
 
     if (text != null) data["text"] = text;
 
-    Firestore.instance.collection("messages").add(data);
+    Firestore.instance.collection("messagens").add(data);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Talk'),
         elevation: 0,
@@ -41,7 +90,7 @@ class _ChatScreenState extends State<ChatScreen> {
         children: [
           Expanded(
               child: StreamBuilder<QuerySnapshot>(
-            stream: Firestore.instance.collection('path').snapshots(),
+            stream: Firestore.instance.collection('messagens').snapshots(),
             builder: (context, snapshot) {
               switch (snapshot.connectionState) {
                 case ConnectionState.none:
@@ -50,14 +99,13 @@ class _ChatScreenState extends State<ChatScreen> {
                     child: CircularProgressIndicator(),
                   );
                 default:
-                  List<DocumentSnapshot> documents = snapshot.data.documents;
+                  List<DocumentSnapshot> documents =
+                      snapshot.data.documents.reversed.toList();
                   return ListView.builder(
                     itemCount: documents.length,
                     reverse: true,
                     itemBuilder: (contex, index) {
-                      return ListTile(
-                        title: Text(documents[index].data['texto']),
-                      );
+                      return ChatMessage(documents[index].data, true);
                     },
                   );
               }
